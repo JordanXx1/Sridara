e44f12bf10f486fadbe67cf36d845139898065e0
JoranXx1 <laster31j@gmail.com>
2023-03-10 14:56:50 +0700

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,245 @@
+<template>
+  <br>
+  <div class="mx-auto max-w-screen-xl px-4 lg:px-1">
+    <div align="right">
+      <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">ใบเสนอราคา</h5>
+      <a href="#" class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+        <div align="left ">
+        <p class="font-normal text-gray-700 dark:text-gray-400">เลขที่&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;QT2022120002</p>
+        <p class="font-normal text-gray-700 dark:text-gray-400">วันที่ &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;19/12/2002</p>
+        <p class="font-normal text-gray-700 dark:text-gray-400">ผู้ขา่ย&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;SRIDARA PRAPRIAW</p>
+        </div>
+      </a>
+    </div>
+  </div>
+<div class="mx-auto max-w-screen-xl px-4 lg:px-1">
+  <div><img class="h-30 w-100" src="https://www.adt.or.th/image/ADT1%20-%201108%20-%200263.jpg" alt="">
+    <p class="font-normal text-gray-700 dark:text-gray-400">บริษัท ศรีดาราทัวร์ จำกัด(สำนักงานใหญ่)</p>
+    <p class="font-normal text-gray-700 dark:text-gray-400">123 ถ.ชยางกูร ม.19 ต.บุ่ง อ.เมือง จ.อำนาจเจริญ 3700</p>
+    <p class="font-normal text-gray-700 dark:text-gray-400">เลขประจำตัวเสียภาษี 0375552000037</p>
+    <p class="font-normal text-gray-700 dark:text-gray-400">เบอร์มือถือ 0823656514</p>
+    <p class="font-normal text-gray-700 dark:text-gray-400">www.facebook.com/sridaratourfanpage</p>
+  </div><br>
+</div>
+
+<div class="mx-auto max-w-screen-xl px-4 lg:px-1">
+    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+      <p>ลูกค้า</p>
+      <p>คุณสมคิด</p>
+        <thead class="text-xs text-gray-700 uppercase bg-gray-100 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+                <th scope="col" class="px-6 py-3 rounded-l-lg">
+                    #
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    รายละเอียด
+                </th>
+                <th scope="col" class="px-6 py-3 rounded-r-lg">
+                    จำนวน
+                </th>
+                <th scope="col" class="px-6 py-3 rounded-r-lg">
+                    ราคาต่อหน่วย
+                </th>
+                <th scope="col" class="px-6 py-3 rounded-r-lg">
+                    ส่วนลด
+                </th>
+                <th scope="col" class="px-6 py-3 rounded-r-lg">
+                    ภาษี
+                </th>
+                <th scope="col" class="px-6 py-3 rounded-r-lg">
+                    มูลค่า
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr class="bg-white dark:bg-gray-800">
+                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    1
+                </th>
+                <td class="px-6 py-4">
+                    ทัวร์อุบล <br>
+                    รถบัสปรับอากาศ 2 คันรวมน้ำมัน <br>
+                    มัคคุเทสก์และทีมงาน <br>
+                    ผู้ช่วยมัคคุเทศก์และทีมงาน <br>
+                    พาหนะเดินทางท่องเที่ยว <br>
+                    ค่าเข้าชมสถานที่ <br>
+                    อาหาร เครื่องดื่ม <br>
+                    โรงแรมที่พัก
+                </td>
+                
+                <td class="px-6 py-4">
+                    117
+                </td>
+                <td class="px-6 py-4">
+                  5,700.00
+                </td>
+                <td class="px-6 py-4">
+                    
+                </td>
+                <td class="px-6 py-4">
+                    7%
+                </td>
+                <td class="px-6 py-4">
+                    666,900.00
+                </td>
+            </tr>
+
+        </tbody>
+    </table>
+    <hr><br>
+    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+        <thead class="font-normal text-gray-700 dark:text-gray-400">
+            <tr>
+              <th scope="col" class="px-6 py-2">
+<br><br>
+<br><br><br><br><br><br>
+(สี่พันหกร้อยบาทถ้วน)</th>
+<th scope="col" class="px-6 py-3">
+ <div align="right">
+    <p class="font-normal text-gray-700 dark:text-gray-400"><b>รวมเป็นเงิน&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;666,900.00 บาท&nbsp;&nbsp;&nbsp;&nbsp;</b></p>
+    <br><p class="font-normal text-gray-700 dark:text-gray-400"><b>มูลค่าที่ไม่มี/ยกเว้นภาษี&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;0.00 บาท&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</b></p>
+    <br><p class="font-normal text-gray-700 dark:text-gray-400"><b>มูลค่าที่คำนวณภาษี &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;623,271.03 บาท&nbsp;&nbsp;&nbsp;&nbsp;</b></p>
+    <br><p class="font-normal text-gray-700 dark:text-gray-400"><b>ภาษีมูลค่าเพิ่ม 7% &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;43,628.97&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</b></p>
+    <br><p class="font-normal text-gray-700 dark:text-gray-400"><b>จำนวนเงินรวทั้งสิ้น&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;666,900.00 บาท&nbsp;&nbsp;&nbsp;&nbsp;</b></p>
+</div>
+                </th>
+            </tr>
+        </thead>
+    </table>
+    <br><br><br><br><br><br><br><br><br><br><br><br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+
+    <div class="relative overflow-x-auto">
+    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+        <thead class="font-normal text-gray-700 dark:text-gray-400">
+            <tr>
+                <th scope="col" class="px-6 py-3">
+                    ในนาม คุณสมคิด
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    
+                  </th>
+                  <th scope="col" class="px-6 py-3">
+                    
+                  </th>
+                  <th scope="col" class="px-6 py-3">
+                    
+                  </th>
+                  <th scope="col" class="px-6 py-3">
+                    
+                  </th>
+                  <th scope="col" class="px-6 py-3">
+                    
+                  </th>
+                  <th scope="col" class="px-6 py-3">
+                    
+                  </th>
+                  <th scope="col" class="px-6 py-3">
+                    
+                  </th>
+                  <th scope="col" class="px-6 py-3">
+                    
+                  </th>
+                  <th scope="col" class="px-6 py-3">
+                    
+                  </th>
+                  <th scope="col" class="px-6 py-3">
+                    
+                  </th>
+                  
+                <th scope="col" class="px-6 py-3">
+                  ในนาม บริษัท ศรีดาราทัวร์ จำกัด
+                </th>
+            </tr>
+        </thead>
+
+    </table>
+</div>
+<br><br><br>
+</div>
+<table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+              <th scope="col" class="px-6 py-2">   
+<div align="right">
+    
+    <br><br><br><br><br>
+   &nbsp;&nbsp;
+   ...................................................
+   &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+   &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+   --------------------------------
+   &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+   
+   <br>
+   ผู้สั่งซื้อสินค้า
+   &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+   &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+   &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+   &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+   วันที่&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+   &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+   </div>
+</th>
+<center><img class="h-15 w-40 " src="https://www.adt.or.th/image/ADT1%20-%201108%20-%200263.jpg" alt=""></center>
+<th scope="col" class="px-6 py-3">
+ <div align="left">
+    
+     <img class="h-15 w-40 " src="https://www.gangbeauty.com/uppic/2017-12/14/3efada8b7.webp" alt="">
+  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+   &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+   &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+   &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+   &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+   &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+   19/12/2022<br>
+  
+   &nbsp;&nbsp;
+   ...................................................
+   &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+   &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+   --------------------------------
+   
+   
+   <br>
+   &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+   ผู้อนุมัติ
+   &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+   &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+   &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+  
+   วันที่
+   </div>
+                </th>
+            </tr>
+        </thead>
+
+</table>
+
+</template>
